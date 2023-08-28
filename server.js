bd17293a49ffe34c078eb5c573c114b9928bc5b0
nawwar daraghmeh <daraghmehnawwar@gmail.com>
2023-08-28 15:54:29 +0300

    creating authentication posts

--- server.js
+++ server.js
@@ -43,3 +43,30 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
+// login post
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/index.ejs",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+// register post
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/register");
+  }
+});
