7562d4ecf79d2958b5f990d150645d163d5f16bf
nawwar daraghmeh <daraghmehnawwar@gmail.com>
2023-08-28 15:54:03 +0300

    creating authentication routes

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 
@@ -13,3 +17,29 @@ initializePassport(
   (email) => users.find((user) => user.email === email),
   (id) => users.find((user) => user.id === id)
 );
+
+const users = [];
+
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("view engine", "ejs");
+
+// route for login page
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+// route for register page
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
