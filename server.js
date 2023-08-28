ddff5ca5da13a4d9a68c0d84b8cd4b5f45e04980
nawwar daraghmeh <daraghmehnawwar@gmail.com>
2023-08-28 15:55:59 +0300

    defining the functionality of url-shortener

--- server.js
+++ server.js
@@ -75,3 +75,36 @@ app.post("/register", async (req, res) => {
 app.get("/index", (req, res) => {
   res.render("index.ejs");
 });
+
+//url shortenr
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(5000);
